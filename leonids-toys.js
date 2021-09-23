ffbd61a5fbdc3b8706030e84623aecd794a23147
Abdurehman05 <abdukr5@gmail.com>
2021-09-23 02:03:45 -0500

    Toy Objects

---
+++ leonids-toys.js
@@ -0,0 +1,12 @@
+const army_men = {
+  name: "Army Men",
+  price: 30
+};
+const peppa_pig = {
+  name: "Peppa Pig",
+  price: 20
+};
+const mickey_mouse = {
+  name: "Mickey Mouse",
+  price: 10
+};
